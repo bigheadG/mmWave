740cacd9b5982b391b645513ae090afdabc87c75
bighead <zcgtaipei@gmail.com>
2020-10-26 16:26:21 +0800

    Update vitalSign_ex0_kv.py

--- VSD/vitalSign_ex0_kv.py
+++ VSD/vitalSign_ex0_kv.py
@@ -36,7 +36,7 @@ except: #pi 2
 #for Ubuntu
 #port = serial.Serial("/dev/ttyACM1",baudrate = 115200, timeout = 0.5)
 
-#for Windows10
+#for Windows 10
 #port = serial.Serial("COM5",baudrate = 115200, timeout = 0.5)
 
 vts = vitalsign_kv.VitalSign_kv(port)
